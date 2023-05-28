9fde6d76d9f11598a79e42f7bed76c82b5bd3940
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-05-28 11:01:00 +0300

    Magazine Initializers, Readers, and Writers

--- lib/Magazine.py
+++ lib/Magazine.py
@@ -1,2 +1,33 @@
 class Magazine:
-    pass
+    def __init__ (self, name, category):
+        self._name = name
+        self._category = category
+
+    @property
+    def name(self):
+        return self._name 
+
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+        
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        self._category = value
+
+
+love = Magazine("Loving", "Relationships")
+# print(love.name)
+love.name = "Hope"
+print(love.name)
+
+# print(love.category)
+love.category = "Sports"
+print(love.category)
+
--- tools/debug.py
+++ tools/debug.py
@@ -21,6 +21,41 @@ if __name__ == '__main__':
     medrine = Author("Joy")
     medrine.name
 
+    class Magazine:
+        def __init__ (self, name, category):
+            self._name = name
+            self._category = category
+
+        @property
+        def name(self):
+            return self._name 
+
+
+        @name.setter
+        def name(self, value):
+            self._name = value
+            
+
+        @property
+        def category(self):
+            return self._category
+
+        @category.setter
+        def category(self, value):
+            self._category = value
+
+
+    love = Magazine("Loving", "Relationships")
+    # print(love.name)
+    love.name = "Hope"
+    print(love.name)
+
+    # print(love.category)
+    love.category = "Sports"
+    print(love.category)
+
+
+
 
 
 
