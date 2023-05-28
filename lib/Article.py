0023014c235378882c0441c1fe36bf89df8bfc87
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-05-28 20:32:55 +0300

    Article Initializers, Readers, and Writers

--- lib/Article.py
+++ lib/Article.py
@@ -1,2 +1,26 @@
+import weakref
+
+
 class Article:
-    pass
+    instances = []
+
+    def __init__(self, author, magazine, title):
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        self.__class__.instances.append(weakref.proxy(self))
+
+
+    @property
+    def title(self):
+        return self._title
+
+
+# Instances of Article
+this_love = Article("Medrine", "Parents", "This Love")
+print(this_love)
+print(this_love.title)
+
+# List of all Article instances
+for instance in Article.instances:
+    print(Article.instances)
--- tools/debug.py
+++ tools/debug.py
@@ -4,6 +4,7 @@ import ipdb
 if __name__ == '__main__':
 #  WRITE YOUR TEST CODE HERE ###
 
+# Author.py tests
     class Author:
 
         def __init__(self, name):
@@ -21,10 +22,18 @@ if __name__ == '__main__':
     medrine = Author("Joy")
     medrine.name
 
-    class Magazine:
+
+gazine.py tests
+    import weakref
+
+
+    class Magazine():
+
+        instances = []
         def __init__ (self, name, category):
             self._name = name
             self._category = category
+            self.__class__.instances.append(weakref.proxy(self))
 
         @property
         def name(self):
@@ -44,8 +53,10 @@ if __name__ == '__main__':
         def category(self, value):
             self._category = value
 
-
+    # Magazine instances
     love = Magazine("Loving", "Relationships")
+
+    faith = Magazine("Faithful", "Worship")
     # print(love.name)
     love.name = "Hope"
     print(love.name)
@@ -53,6 +64,44 @@ if __name__ == '__main__':
     # print(love.category)
     love.category = "Sports"
     print(love.category)
+
+
+    # List of all Magazine Instances
+    for instance in Magazine.instances:
+        # print(instance.name)
+        # print(instance.category)
+        print(Magazine.instances)
+
+
+# Article.py tests
+
+    import weakref
+
+
+    class Article:
+        instances = []
+
+        def __init__(self, author, magazine, title):
+            self._author = author
+            self._magazine = magazine
+            self._title = title
+            self.__class__.instances.append(weakref.proxy(self))
+
+
+        @property
+        def title(self):
+            return self._title
+
+
+    # Instances of Article
+    this_love = Article("Medrine", "Parents", "This Love")
+    print(this_love)
+    print(this_love.title)
+
+    # List of all Article instances
+    for instance in Article.instances:
+        print(Article.instances)
+
 
 
 
