b022d5411cc463356650d46e6b59fca116dd3ce1
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-05-29 08:33:40 +0300

    Author Object Relationship Methods

--- lib/Author.py
+++ lib/Author.py
@@ -2,6 +2,7 @@ class Author:
 
     def __init__(self, name):
         self._name = name
+        self.article_list = []
 
     @property
     def name(self):
@@ -10,6 +11,17 @@ class Author:
     @name.setter
     def name(self):
         pass
+
+    @property
+    def articles(self):
+        return self._articles
+
+    @property
+    def magazines(self):
+        unique_magazine = set()
+        for article in self.article_list:
+            unique_magazine.add(article.magazine)
+        return list(unique_magazine)
 
 
 medrine = Author("Joy")
